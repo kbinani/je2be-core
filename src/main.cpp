61224a244d4427eac947badc3a0aa0751ba1c71a
kbinani <kbinani.bt@gmail.com>
2020-10-15 05:02:32 +0900

    feat: Open db dir

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,41 @@
 #include <minecraft-file.hpp>
 #include <leveldb/db.h>
+#include <leveldb/zlib_compressor.h>
+#include <leveldb/decompress_allocator.h>
 #include <string>
 #include <iostream>
 
 int main(int argc, char *argv[]) {
     using namespace std;
+    using namespace leveldb;
 
-    if (argc != 3) {
+    if (argc != 2) {
         return -1;
     }
-    string const input = argv[1];
-    string const output = argv[2];
+    string const dir = argv[1];
 
-    leveldb::DB *db;
-    leveldb::Options options;
-    leveldb::Status status = leveldb::DB::Open(options, input.c_str(), &db);
+    DB *db;
+    Options options;
+    options.compressors[0] = new ZlibCompressorRaw(-1);
+    options.compressors[1] = new ZlibCompressor();
+    Status status = DB::Open(options, dir.c_str(), &db);
     if (!status.ok()) {
         cerr << status.ToString() << endl;
         return -1;
     }
 
-    //TODO:
+    cout << "DB::Open ok" << endl;
+
+    ReadOptions ro;
+    ro.decompress_allocator = new DecompressAllocator();
+    
+    leveldb::Iterator *it = db->NewIterator(ro);
+    for (it->SeekToFirst(); it->Valid(); it->Next()) {
+        cout << it->key().ToString() << endl;
+    }
+    delete it;
+
+    delete db;
 
     return 0;
 }
