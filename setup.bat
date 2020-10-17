a2291ce6830d7a9bd41036601e1d5416272c565d
kbinani <kbinani.bt@gmail.com>
2020-10-18 01:02:54 +0900

    chore: Enable out-of-source build for Windows

---
+++ setup.bat
@@ -0,0 +1,35 @@
+rem This is a setup script for out-of-source build.
+rem Call this script in your build directory.
+
+rem Example:
+rem cd C:\your\build\directory
+rem C:\location\of\this\script\setup.bat
+rem start je2be.sln
+
+@echo off
+setlocal enabledelayedexpansion
+
+mkdir deps
+pushd deps
+
+set dir=%~dp0
+
+rem zlib
+mkdir zlib
+pushd zlib
+cmake %dir%\ext\zlib
+cmake --build . --config Debug
+cmake --build . --config Release
+popd
+
+rem leveldb
+mkdir leveldb
+pushd leveldb
+cmake %dir%\ext\leveldb -DZLIB_LIBRARY=..\zlib\Release\zlib -DZLIB_INCLUDE_DIR=..\zlib
+cmake --build . --target leveldb --config Debug
+cmake --build . --target leveldb --config Release
+popd
+
+cmake %dir%
+
+popd
