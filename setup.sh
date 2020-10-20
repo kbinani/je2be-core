2af5f6de3c2f0779c7d351fabbebd33b41faed6e
kbinani <kbinani.bt@gmail.com>
2020-10-21 03:32:43 +0900

    feat: Build xxHash for UNIX-like systems

--- setup.sh
+++ setup.sh
@@ -14,6 +14,18 @@ cmake --build . --target leveldb --config Debug
 cmake --build . --target leveldb --config Release
 popd
 
+mkdir -p deps/xxHash
+pushd deps/xxHash
+
+if [ -z "$generator" ]; then
+	cmake "$dir/ext/xxHash/cmake_unofficial" -DBUILD_SHARED_LIBS=OFF
+else
+	cmake "$dir/ext/xxHash/cmake_unofficial" -DBUILD_SHARED_LIBS=OFF -G "$generator"
+fi
+cmake --build . --target xxhash --config Debug
+cmake --build . --target xxhash --config Release
+popd
+
 if [ -z "$generator" ]; then
 	cmake "$dir"
 else
