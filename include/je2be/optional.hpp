1b3f787de3a935c598f75f9318eac7c042f86734
kbinani <kbinani.bt@gmail.com>
2020-11-11 02:18:03 +0900

    feat: Templating the Wrap function

--- include/je2be/optional.hpp
+++ include/je2be/optional.hpp
@@ -2,7 +2,7 @@
 
 namespace j2b {
 
-inline int32_t Wrap(std::optional<int32_t> v, int32_t fallback) {
+template <class T> inline T Wrap(std::optional<T> v, T fallback) {
   return v ? *v : fallback;
 }
 
