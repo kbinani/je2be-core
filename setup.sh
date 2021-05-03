113cdb959462c64620812ab83a72090c8366dd7a
kbinani <kbinani.bt@gmail.com>
2021-05-04 05:02:38 +0900

    fix: Linking failed when with Makefile generator

--- setup.sh
+++ setup.sh
@@ -6,22 +6,34 @@ dir="$(cd "$(dirname "$0")"; pwd)"
 mkdir -p deps/leveldb
 pushd deps/leveldb
 if [ -z "$generator" ]; then
-	cmake "$dir/ext/leveldb" -DLEVELDB_SNAPPY=OFF
+	for type in Debug Release; do
+		mkdir $type
+		pushd $type
+		cmake "$dir/ext/leveldb" -DLEVELDB_SNAPPY=OFF -DCMAKE_BUILD_TYPE=$type
+		cmake --build . --target leveldb --config $type
+		popd
+	done
 else
 	cmake "$dir/ext/leveldb" -DLEVELDB_SNAPPY=OFF -G "$generator"
+	cmake --build . --target leveldb --config Debug
+	cmake --build . --target leveldb --config Release
 fi
-cmake --build . --target leveldb --config Debug
-cmake --build . --target leveldb --config Release
 popd
 
 mkdir -p deps/xxHash
 pushd deps/xxHash
 
 if [ -z "$generator" ]; then
-	cmake "$dir/ext/xxHash/cmake_unofficial" -DBUILD_SHARED_LIBS=OFF
+	for type in Debug Release; do
+		mkdir $type
+		pushd $type
+		cmake "$dir/ext/xxHash/cmake_unofficial" -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=$type
+		cmake --build . --target xxhash --config $type
+		popd
+	done
 else
 	cmake "$dir/ext/xxHash/cmake_unofficial" -DBUILD_SHARED_LIBS=OFF -G "$generator"
+	cmake --build . --target xxhash --config Debug
+	cmake --build . --target xxhash --config Release
 fi
-cmake --build . --target xxhash --config Debug
-cmake --build . --target xxhash --config Release
 popd
