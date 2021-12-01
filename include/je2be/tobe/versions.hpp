61fbe77521667f344447b425a62235a5f3bd07ed
kbinani <kbinani.bt@gmail.com>
2021-12-01 14:29:12 +0900

    feat: Update version constants for 1.18

--- include/je2be/tobe/versions.hpp
+++ include/je2be/tobe/versions.hpp
@@ -4,14 +4,14 @@ namespace je2be::tobe {
 namespace {
 
 int32_t constexpr kBlockDataVersion = 17879555;
-Version constexpr kMinimumCompatibleClientVersion = Version(1, 17, 11, 0, 0);
-char constexpr kSubChunkVersion = 0x16;
-uint8_t constexpr kSubChunkBlockStorageVersion = 8;
-int32_t constexpr kNetworkVersion = 448;
-char const *const kInventoryVersion = "1.17.11";
+Version constexpr kMinimumCompatibleClientVersion = Version(1, 18, 0, 0, 0);
+char constexpr kSubChunkVersion = 0x27;
+uint8_t constexpr kSubChunkBlockStorageVersion = 9;
+int32_t constexpr kNetworkVersion = 475;
+char const *const kInventoryVersion = "1.18.0";
 uint8_t constexpr kDragonFightVersion = 0;
 
-Version constexpr kSupportVersion = Version(1, 17, 11, 1, 0);
+Version constexpr kSupportVersion = Version(1, 18, 0, 2, 0);
 
 } // namespace
 } // namespace je2be::tobe
