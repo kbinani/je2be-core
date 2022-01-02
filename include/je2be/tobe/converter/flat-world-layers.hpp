a8998ce445ef152c7bc232b08fd0df24608f9c29
kbinani <kbinani.bt@gmail.com>
2022-01-03 04:28:31 +0900

    feat: Switch encoding_version of FlatWorldLayers

--- include/je2be/tobe/converter/flat-world-layers.hpp
+++ include/je2be/tobe/converter/flat-world-layers.hpp
@@ -7,15 +7,75 @@ class FlatWorldLayers {
 public:
   static std::optional<std::string> FromLevelData(mcfile::nbt::CompoundTag const &data) {
     using namespace std;
-    auto version = data.int32("version");
-    if (!version) {
-      return nullopt;
-    }
+    auto generatorOptions = data.compoundTag("generatorOptions");
     auto worldGenSettings = data.compoundTag("WorldGenSettings");
-    if (!worldGenSettings) {
+    auto generatorName = data.string("generatorName");
+    auto version = data.int32("DataVersion");
+    if (worldGenSettings && version) {
+      auto ret = UsingWorldGenSettings(*worldGenSettings, *version);
+      if (ret) {
+        return ret;
+      }
+    }
+    if (generatorOptions) {
+      auto ret = UsingGeneratorOptions(*generatorOptions);
+      if (ret) {
+        return ret;
+      }
+    }
+    if (generatorName == "flat") {
+      return R"({"biome_id":1,"block_layers":[{"block_name":"minecraft:bedrock","count":1},{"block_name":"minecraft:dirt","count":2},{"block_name":"minecraft:grass","count":1}],"encoding_version":5,"structure_options":null})" + string("\x0a");
+    }
+    return nullopt;
+  }
+
+  static std::optional<std::string> UsingGeneratorOptions(mcfile::nbt::CompoundTag const &generatorOptions) {
+    using namespace std;
+    auto layers = generatorOptions.listTag("layers");
+    auto biomeString = generatorOptions.string("biome");
+    if (!layers || !biomeString) {
       return nullopt;
     }
-    auto dimensions = worldGenSettings->compoundTag("dimensions");
+    auto biome = mcfile::biomes::FromName(*biomeString);
+    if (!biome) {
+      return nullopt;
+    }
+    nlohmann::json obj;
+    obj["biome_id"] = mcfile::be::Biome::ToUint32(*biome);
+    obj["block_layers"].array();
+    for (auto const &layer : *layers) {
+      auto c = layer->asCompound();
+      if (!c) {
+        return nullopt;
+      }
+      auto blockString = c->string("block");
+      auto height = c->byte("height");
+      if (!blockString || !height) {
+        return nullopt;
+      }
+      auto block = make_shared<mcfile::je::Block const>(*blockString);
+      auto converted = BlockData::From(block);
+      if (!converted) {
+        return nullopt;
+      }
+      auto name = converted->string("name");
+      if (!name) {
+        return nullopt;
+      }
+      nlohmann::json item;
+      item["block_name"] = *name;
+      item["count"] = *height;
+      obj["block_layers"].push_back(item);
+    }
+    obj["encoding_version"] = 5;
+    obj["structure_options"] = nlohmann::json::value_t::null;
+
+    return nlohmann::to_string(obj) + "\x0a";
+  }
+
+  static std::optional<std::string> UsingWorldGenSettings(mcfile::nbt::CompoundTag const &worldGenSettings, int dataVersion) {
+    using namespace std;
+    auto dimensions = worldGenSettings.compoundTag("dimensions");
     if (!dimensions) {
       return nullopt;
     }
@@ -70,14 +130,19 @@ public:
       if (!name) {
         return nullopt;
       }
-      nlohmann::json layer;
-      layer["block_name"] = *name;
-      layer["count"] = *height;
-      obj["block_layers"].push_back(layer);
+      nlohmann::json item;
+      item["block_name"] = *name;
+      item["count"] = *height;
+      obj["block_layers"].push_back(item);
     }
-    obj["encoding_version"] = 6;
     obj["structure_options"] = nlohmann::json::value_t::null;
-    obj["world_version"] = "version.post_1_18";
+
+    if (dataVersion < 2834) {
+      obj["encoding_version"] = 5;
+    } else {
+      obj["encoding_version"] = 6;
+      obj["world_version"] = "version.post_1_18";
+    }
 
     return nlohmann::to_string(obj) + "\x0a";
   }
