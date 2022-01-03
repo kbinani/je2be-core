2e0b26fb8d046afd041f44b9572f9a84da2bacdd
kbinani <kbinani.bt@gmail.com>
2022-01-04 00:36:07 +0900

    feat: Import superflat config version 2

--- include/je2be/tobe/converter/flat-world-layers.hpp
+++ include/je2be/tobe/converter/flat-world-layers.hpp
@@ -24,8 +24,8 @@ public:
         return ret;
       }
     }
-    if (stringGeneratorOptions && version) {
-      auto ret = UsingStringGeneratorOptions(*stringGeneratorOptions, *version);
+    if (stringGeneratorOptions) {
+      auto ret = UsingStringGeneratorOptions(*stringGeneratorOptions, version ? *version : 100);
       if (ret) {
         return ret;
       }
@@ -37,7 +37,6 @@ public:
   }
 
   static std::optional<std::string> UsingStringGeneratorOptions(std::string const &generatorOptions, int dataVersion) {
-    // 3;minecraft:bedrock,230*minecraft:stone,5*minecraft:dirt,minecraft:grass;3;biome_1,decoration,stronghold,mineshaft,dungeon
     using namespace std;
     using namespace mcfile;
 
@@ -45,8 +44,33 @@ public:
     if (idx == string::npos) {
       return nullopt;
     }
+    string version;
+    version.assign(generatorOptions.begin(), generatorOptions.begin() + idx);
+    string trailing = generatorOptions.substr(idx + 1);
+    if (version == "2") {
+      return UsingStringGeneratorOptionsV2(trailing, dataVersion);
+    } else if (version == "3") {
+      return UsingStringGeneratorOptionsV3(trailing, dataVersion);
+    }
+  }
+
+  static std::optional<std::string> UsingStringGeneratorOptionsV3(std::string const &options, int dataVersion) {
+    // 3;minecraft:bedrock,230*minecraft:stone,5*minecraft:dirt,minecraft:grass;3;biome_1,decoration,stronghold,mineshaft,dungeon
+
+    using namespace std;
+    using namespace mcfile;
+
+    auto idxBiomeStart = options.find(';');
+    if (idxBiomeStart == string::npos) {
+      return nullopt;
+    }
     string biomeString;
-    biomeString.assign(generatorOptions.begin(), generatorOptions.begin() + idx);
+    auto idxBiomeEnd = options.find(';', idxBiomeStart + 1);
+    if (idxBiomeEnd == string::npos) {
+      biomeString = options.substr(idxBiomeStart + 1);
+    } else {
+      biomeString.assign(options.begin() + idxBiomeStart + 1, options.begin() + idxBiomeEnd);
+    }
     auto biomeInt = strings::Toi(biomeString);
     if (!biomeInt) {
       return nullopt;
@@ -60,9 +84,7 @@ public:
     obj["biome_id"] = mcfile::be::Biome::ToUint32(biome);
     obj["block_layers"] = nlohmann::json::value_t::array;
 
-    string layersString;
-    auto next = generatorOptions.find(';', idx + 1);
-    layersString.assign(generatorOptions.begin() + idx + 1, next == string::npos ? generatorOptions.end() : generatorOptions.begin() + next);
+    string layersString = options.substr(0, idxBiomeStart);
 
     for (string const &option : String::Split(layersString, ',')) {
       auto x = option.find('*');
@@ -78,6 +100,86 @@ public:
         blockString = option.substr(x + 1);
       }
       auto name = BedrockBlockName(blockString, dataVersion);
+      if (!name) {
+        return nullopt;
+      }
+      nlohmann::json item;
+      item["block_name"] = *name;
+      item["count"] = count;
+      obj["block_layers"].push_back(item);
+    }
+
+    obj["encoding_version"] = 5;
+    obj["structure_options"] = nlohmann::json::value_t::null;
+
+    return nlohmann::to_string(obj) + "\x0a";
+  }
+
+  static std::optional<std::string> UsingStringGeneratorOptionsV2(std::string const &options, int dataVersion) {
+    // 2;7,2x3:2,2;1;village
+    using namespace std;
+    using namespace mcfile;
+    auto idxBiomeStart = options.find(';');
+    if (idxBiomeStart == string::npos) {
+      return nullopt;
+    }
+    string biomeString;
+    auto idxBiomeEnd = options.find(';', idxBiomeStart + 1);
+    if (idxBiomeEnd == string::npos) {
+      biomeString = options.find(idxBiomeStart + 1);
+    } else {
+      biomeString.assign(options.begin() + idxBiomeStart + 1, options.begin() + idxBiomeEnd);
+    }
+    auto biomeInt = strings::Toi(biomeString);
+    if (!biomeInt) {
+      return nullopt;
+    }
+    auto biome = mcfile::biomes::FromInt(*biomeInt);
+    if (biome == biomes::unknown) {
+      return nullopt;
+    }
+
+    nlohmann::json obj;
+    obj["biome_id"] = mcfile::be::Biome::ToUint32(biome);
+    obj["block_layers"] = nlohmann::json::value_t::array;
+
+    string layersString = options.substr(0, idxBiomeStart);
+    for (string const &option : String::Split(layersString, ',')) {
+      int count = 1;
+      auto x = option.find('x');
+      string blockString = option;
+      if (x != string::npos) {
+        string countString = option.substr(0, x);
+        auto countInt = strings::Toi(countString);
+        if (!countInt) {
+          return nullopt;
+        }
+        count = *countInt;
+        blockString = option.substr(x + 1);
+      }
+      auto colon = blockString.find(':');
+      int data = 0;
+      if (colon != string::npos) {
+        auto maybeData = strings::Toi(blockString.substr(colon + 1));
+        if (!maybeData) {
+          return nullopt;
+        }
+        blockString = blockString.substr(0, colon);
+        data = *maybeData;
+      }
+      auto blockId = strings::Toi(blockString);
+      if (!blockId) {
+        return nullopt;
+      }
+      auto block = mcfile::je::Flatten::DoFlatten(*blockId, data);
+      if (!block) {
+        return nullopt;
+      }
+      auto converted = BlockData::From(block);
+      if (!converted) {
+        return nullopt;
+      }
+      auto name = converted->string("name");
       if (!name) {
         return nullopt;
       }
@@ -113,9 +215,19 @@ public:
         return nullopt;
       }
       auto blockString = c->string("block");
-      auto height = c->byte("height");
-      if (!blockString || !height) {
+      if (!blockString) {
         return nullopt;
+      }
+      int height = 1;
+      auto heightByte = c->byte("height");
+      if (heightByte) {
+        height = *heightByte;
+      } else {
+        auto heightInt16 = c->int16("height");
+        if (!heightInt16) {
+          return nullopt;
+        }
+        height = *heightInt16;
       }
       auto name = BedrockBlockName(*blockString, dataVersion);
       if (!name) {
@@ -123,7 +235,7 @@ public:
       }
       nlohmann::json item;
       item["block_name"] = *name;
-      item["count"] = *height;
+      item["count"] = height;
       obj["block_layers"].push_back(item);
     }
     obj["encoding_version"] = 5;
