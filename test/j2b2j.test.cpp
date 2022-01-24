c313db1bf0a9045decf4de9118f0cdf69d257cda
kbinani <kbinani.bt@gmail.com>
2022-01-25 05:01:02 +0900

    feat: Test java -> bedrock -> java conversion

--- test/j2b2j.test.cpp
+++ test/j2b2j.test.cpp
@@ -34,5 +34,59 @@ TEST_CASE("j2b2j") {
   CHECK(toje.run(thread::hardware_concurrency()));
 
   // Compare initial Java input and final Java output.
-  //TODO:
+  for (auto dim : {mcfile::Dimension::Overworld, mcfile::Dimension::Nether, mcfile::Dimension::End}) {
+    if (!io.fDimensionFilter.empty()) {
+      if (io.fDimensionFilter.find(dim) == io.fDimensionFilter.end()) {
+        continue;
+      }
+    }
+    auto regionDirA = io.getWorldDirectory(*outJ, dim) / "region";
+    auto regionDirE = io.getWorldDirectory(in, dim) / "region";
+    for (auto it : fs::directory_iterator(regionDirA)) {
+      if (!it.is_regular_file()) {
+        continue;
+      }
+      auto fileA = it.path();
+      if (fileA.extension() != ".mca") {
+        continue;
+      }
+      auto regionA = mcfile::je::Region::MakeRegion(fileA);
+      CHECK(regionA);
+
+      auto fileE = regionDirE / fileA.filename();
+      auto regionE = mcfile::je::Region::MakeRegion(fileE);
+      CHECK(regionE);
+
+      for (int cz = regionA->minChunkZ(); cz <= regionA->maxChunkZ(); cz++) {
+        for (int cx = regionA->minChunkX(); cx <= regionA->maxChunkX(); cx++) {
+          if (!io.fChunkFilter.empty()) {
+            if (io.fChunkFilter.find(Pos2i(cx, cz)) == io.fChunkFilter.end()) {
+              continue;
+            }
+          }
+          auto chunkA = regionA->chunkAt(cx, cz);
+          if (!chunkA) {
+            continue;
+          }
+          auto chunkE = regionE->chunkAt(cx, cz);
+          CHECK(chunkE);
+
+          CHECK(chunkA->minBlockY() == chunkE->minBlockY());
+          CHECK(chunkA->maxBlockY() == chunkE->maxBlockY());
+
+          for (int y = chunkE->minBlockY(); y <= chunkE->maxBlockY(); y++) {
+            for (int z = chunkE->minBlockZ(); z <= chunkE->maxBlockZ(); z++) {
+              for (int x = chunkE->minBlockX(); x <= chunkE->maxBlockX(); x++) {
+                auto blockA = chunkA->blockAt(x, y, z);
+                auto blockE = chunkE->blockAt(x, y, z);
+                CHECK(blockA);
+                CHECK(blockE);
+                CHECK(blockA->fName == blockE->fName);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
