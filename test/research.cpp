f080f9f7627c2d767e60a1e9d0b56b826ebbd70e
kbinani <kbinani.bt@gmail.com>
2022-01-26 19:27:51 +0900

    feat: Researches for connectable blocks to fence, glass pane, and iron bars

--- test/research.cpp
+++ test/research.cpp
@@ -2,6 +2,7 @@
 
 #include <je2be.hpp>
 
+#include <fstream>
 #include <iostream>
 
 #if defined(_WIN32)
@@ -75,7 +76,102 @@ void VisitDb(string const &name, function<void(string const &key, string const &
   });
 }
 
+static void FenceGlassPaneIronBarsConnectable() {
+  set<string> uniq;
+  for (mcfile::blocks::BlockId id = 1; id < mcfile::blocks::minecraft::minecraft_max_block_id; id++) {
+    string name = mcfile::blocks::Name(id);
+    if (name.ends_with("_stairs")) {
+      continue;
+    }
+    if (name.ends_with("piston")) {
+      continue;
+    }
+    if (name.ends_with("door")) {
+      continue;
+    }
+    uniq.insert(name);
+  }
+  vector<string> names(uniq.begin(), uniq.end());
+
+  int const x0 = -42;
+  int const z0 = 165;
+  int const y = 4;
+  int x = x0;
+  int x1 = x0;
+  fs::path root("C:/Users/kbinani/AppData/Roaming/.minecraft/saves/labo");
+  {
+    ofstream os((root / "datapacks" / "kbinani" / "data" / "je2be" / "functions" / "place_blocks.mcfunction").string());
+    for (string const &name : names) {
+      os << "setblock " << x << " " << y << " " << z0 << " " << name << endl;
+      x++;
+    }
+    x1 = x;
+    os << "fill " << x0 << " " << y << " " << (z0 + 1) << " " << x1 << " " << y << " " << (z0 + 1) << " glass_pane" << endl;
+    os << "fill " << x0 << " " << y << " " << (z0 - 1) << " " << x1 << " " << y << " " << (z0 - 1) << " oak_fence" << endl;
+  }
+
+  // login the game, then execute /function je2be:place_blocks
+
+  mcfile::je::World w(root);
+  shared_ptr<mcfile::je::Chunk> chunk;
+  int cz = mcfile::Coordinate::ChunkFromBlock(z0);
+  set<string> glassPaneAttachable;
+  set<string> fenceAttachable;
+  for (int x = x0; x < x1; x++) {
+    int cx = mcfile::Coordinate::ChunkFromBlock(x);
+    if (!chunk || (chunk && chunk->fChunkX != cx)) {
+      chunk = w.chunkAt(cx, cz);
+    }
+    auto center = chunk->blockAt(x, y, z0);
+    auto expected = names[x - x0];
+    if (expected != center->fName) {
+      cerr << "block does not exist: expected=" << expected << "; actual=" << center->fName << endl;
+    } else {
+      auto fence = chunk->blockAt(x, y, z0 - 1);
+      auto fenceAttached = fence->property("south") == "true";
+      if (fenceAttached) {
+        fenceAttachable.insert(expected);
+      }
+
+      auto glassPane = chunk->blockAt(x, y, z0 + 1);
+      auto glassPaneAttached = glassPane->property("north") == "true";
+      if (glassPaneAttached) {
+        glassPaneAttachable.insert(expected);
+      }
+    }
+  }
+
+  fs::path self = fs::path(__FILE__).parent_path();
+  ofstream code((self / "code.hpp").string());
+  code << "static bool IsFenceAlwaysAttachable(mcfile::blocks::BlockId id) {" << endl;
+  code << "  switch (id) {" << endl;
+  for (auto n : fenceAttachable) {
+    code << "    case mcfile::blocks::minecraft::" << n.substr(10) << ":" << endl;
+  }
+  code << "      return true;" << endl;
+  code << "    default:" << endl;
+  code << "      break;" << endl;
+  code << "  }" << endl;
+  code << "  //TODO:" << endl;
+  code << "  return false;" << endl;
+  code << "}" << endl;
+  code << endl;
+  code << "static bool IsGlassPaneOrIronBarsAlwaysAttachable(mcfile::blocks::BlockId id) {" << endl;
+  code << "  switch (id) {" << endl;
+  for (auto n : glassPaneAttachable) {
+    code << "    case mcfile::blocks::minecraft::" << n.substr(10) << ":" << endl;
+  }
+  code << "      return true;" << endl;
+  code << "    default:" << endl;
+  code << "      break;" << endl;
+  code << "  }" << endl;
+  code << "  //TODO:" << endl;
+  code << "  return false;" << endl;
+  code << "}" << endl;
+}
+
 } // namespace
 
 TEST_CASE("research") {
+  FenceGlassPaneIronBarsConnectable();
 }
