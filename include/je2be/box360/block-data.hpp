4c1f974756c8f47a6a0c423108ba0601a209672b
kbinani <kbinani.bt@gmail.com>
2022-03-13 21:22:03 +0900

    feat: Add BlockData::StrippedLog

--- include/je2be/box360/block-data.hpp
+++ include/je2be/box360/block-data.hpp
@@ -3,22 +3,34 @@
 namespace je2be::box360 {
 
 struct BlockData : std::tuple<uint8_t, uint8_t> {
-  BlockData(uint8_t blockId, uint8_t data) : std::tuple<uint8_t, uint8_t>(blockId, data) {}
+  BlockData(uint8_t rawBlockId, uint8_t data) : std::tuple<uint8_t, uint8_t>(rawBlockId, data) {}
   BlockData() : std::tuple<uint8_t, uint8_t>(0, 0) {}
 
-  uint16_t blockId() const {
+  uint16_t rawBlockId() const {
     return std::get<0>(*this);
   }
 
-  uint8_t data() const {
+  uint8_t rawData() const {
     return std::get<1>(*this);
   };
+
+  uint16_t extendedBlockId() const {
+    return (((uint16_t)rawData() & 0x70) << 4) | rawBlockId();
+  }
+
+  uint8_t data() const {
+    return rawData() & 0xf;
+  }
+
+  bool isWaterlogged() const {
+    return (rawData() & 0x80) == 0x80;
+  }
 
   std::shared_ptr<mcfile::je::Block const> toBlock() const {
     using namespace std;
     auto p = unsafeToBlock();
     if (p) {
-      if ((data() & 0x80) == 0x80) {
+      if (isWaterlogged()) {
         map<string, string> props(p->fProperties);
         props["waterlogged"] = "true";
         return make_shared<mcfile::je::Block const>(p->fName, props);
@@ -30,178 +42,100 @@ struct BlockData : std::tuple<uint8_t, uint8_t> {
     }
   }
 
+  static void StrippedLog(uint8_t data, std::map<std::string, std::string> &props) {
+    switch (data) {
+    case 0x4:
+      props["axis"] = "x";
+      break;
+    case 0x8:
+      props["axis"] = "z";
+      break;
+    case 0x0:
+    default:
+      props["axis"] = "y";
+      break;
+    }
+  }
+
   std::shared_ptr<mcfile::je::Block const> unsafeToBlock() const {
     using namespace std;
-    uint8_t id = blockId();
-    uint8_t rawData = this->data();
-    if ((rawData & 0x70) == 0) {
-      return mcfile::je::Flatten::DoFlatten(id, 0xf & rawData);
+    uint8_t rawId = rawBlockId();
+    uint8_t rawData = this->rawData();
+    uint16_t id = extendedBlockId();
+    uint8_t data = this->data();
+    if (rawId == id) {
+      return mcfile::je::Flatten::DoFlatten(id, data);
     }
     string name;
     map<string, string> props;
-    uint8_t data = rawData & 0x7f;
     switch (id) {
-    case 2:
-      switch (data) {
-      case 0x10:
-        name = "kelp_plant";
-        break;
-      }
+    case 258:
+      name = "kelp_plant";
       break;
-    case 13:
+    case 269:
       switch (data) {
-      case 16:
+      case 0x6:
         name = "dried_kelp_block";
         break;
       }
       break;
-    case 14:
+    case 270:
       switch (data) {
-      case 0x10:
+      case 0:
         name = "seagrass";
         break;
-      case 0x11:
+      case 1:
         name = "tall_seagrass";
         props["half"] = "upper";
         break;
-      case 0x12:
+      case 2:
         name = "tall_seagrass";
         props["half"] = "lower";
         break;
       }
       break;
-    case 16:
+    case 272:
       switch (data) {
-      case 0x11:
+      case 1:
         name = "bubble_column";
         break;
       }
       break;
-    case 17:
-      switch (data) {
-      case 0x10:
-        name = "blue_ice";
-        break;
-      }
+    case 273:
+      name = "blue_ice";
       break;
-    case 18:
-      switch (data) {
-      case 0x11:
-        name = "spruce_trap_door";
-        break;
-      }
+    case 274:
+      name = "spruce_trapdoor";
+      mcfile::je::Flatten::Trapdoor(data, props);
       break;
-    case 39:
-      switch (data) {
-      case 0x10:
-        name = "stripped_spruce_log";
-        props["axis"] = "y";
-        break;
-      case 0x14:
-        name = "stripped_spruce_log";
-        props["axis"] = "x";
-        break;
-      case 0x18:
-        name = "stripped_spruce_log";
-        props["axis"] = "z";
-        break;
-      }
+    case 295:
+      name = "stripped_spruce_log";
+      StrippedLog(data, props);
       break;
-    case 40:
-      switch (data) {
-      case 0x10:
-        name = "stripped_birch_log";
-        props["axis"] = "y";
-        break;
-      case 0x14:
-        name = "stripped_birch_log";
-        props["axis"] = "x";
-        break;
-      case 0x18:
-        name = "stripped_birch_log";
-        props["axis"] = "z";
-        break;
-      }
+    case 296:
+      name = "stripped_birch_log";
+      StrippedLog(data, props);
       break;
-    case 41:
-      switch (data) {
-      case 0x10:
-        name = "stripped_jungle_log";
-        props["axis"] = "y";
-        break;
-      case 0x14:
-        name = "stripped_jungle_log";
-        props["axis"] = "x";
-        break;
-      case 0x18:
-        name = "stripped_jungle_log";
-        props["axis"] = "z";
-        break;
-      }
+    case 297:
+      name = "stripped_jungle_log";
+      StrippedLog(data, props);
       break;
-    case 42:
-      switch (data) {
-      case 0x10:
-        name = "stripped_acacia_log";
-        props["axis"] = "y";
-        break;
-      case 0x14:
-        name = "stripped_acacia_log";
-        props["axis"] = "x";
-        break;
-      case 0x18:
-        name = "stripped_acacia_log";
-        props["axis"] = "z";
-        break;
-      }
+    case 298:
+      name = "stripped_acacia_log";
+      StrippedLog(data, props);
       break;
-    case 43:
-      switch (data) {
-      case 0x10:
-        name = "stripped_dark_oak_log";
-        props["axis"] = "y";
-        break;
-      case 0x14:
-        name = "stripped_dark_oak_log";
-        props["axis"] = "x";
-        break;
-      case 0x18:
-        name = "stripped_dark_oak_log";
-        props["axis"] = "z";
-        break;
-      }
+    case 299:
+      name = "stripped_dark_oak_log";
+      StrippedLog(data, props);
       break;
-    case 44:
-      switch (data) {
-      case 0x00:
-        name = "smooth_stone_slab";
-        props["type"] = "double";
-        break;
-      case 0x02:
-        name = "smooth_stone_slab";
-        props["type"] = "bottom";
-        break;
-      case 0x08:
-        name = "smooth_stone_slab";
-        props["type"] = "top";
-        break;
-      case 0x10:
-        name = "stripped_oak_log";
-        props["axis"] = "y";
-        break;
-      case 0x14:
-        name = "stripped_oak_log";
-        props["axis"] = "x";
-        break;
-      case 0x18:
-        name = "stripped_oak_log";
-        props["axis"] = "z";
-        break;
-      }
+    case 300:
+      name = "stripped_oak_log";
+      StrippedLog(data, props);
       break;
     }
     if (name.empty()) {
-      return mcfile::je::Flatten::DoFlatten(id, 0xf & data);
+      auto b = mcfile::je::Flatten::DoFlatten(rawId, data);
+      return b;
     } else {
       return make_shared<mcfile::je::Block const>("minecraft:" + name, props);
     }
