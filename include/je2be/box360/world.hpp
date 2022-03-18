7f94309f3d1698518c643d4a3bdfcacb49f0392e
kbinani <kbinani.bt@gmail.com>
2022-03-18 18:08:45 +0900

    feat: ConcatCompressNbt in parallel

--- include/je2be/box360/world.hpp
+++ include/je2be/box360/world.hpp
@@ -79,6 +79,10 @@ public:
       for (auto &f : futures) {
         ok = ok && f.get();
       }
+      futures.clear();
+    }
+    if (!ok) {
+      return false;
     }
 
     if (!Terraform::Do(*temp, concurrency)) {
@@ -88,18 +92,32 @@ public:
     if (!Fs::CreateDirectories(outputDirectory / pathToRegion)) {
       return false;
     }
+
+    mcfile::je::Region::ConcatOptions o;
+    o.fDeleteInput = true;
     for (int rz = -1; rz <= 0; rz++) {
       for (int rx = -1; rx <= 0; rx++) {
         auto mca = outputDirectory / pathToRegion / mcfile::je::Region::GetDefaultRegionFileName(rx, rz);
-        if (!mcfile::je::Region::ConcatCompressedNbt(rx, rz, *temp, mca)) {
-          return false;
+        if (queue) {
+          futures.push_back(move(queue->enqueue(ConcatCompressedNbt, rx, rz, *temp, mca, o)));
+        } else {
+          if (!ConcatCompressedNbt(rx, rz, *temp, mca, o)) {
+            return false;
+          }
         }
       }
     }
-    return true;
+    for (auto &f : futures) {
+      ok = ok && f.get();
+    }
+    return ok;
   }
 
 private:
+  static bool ConcatCompressedNbt(int rx, int rz, std::filesystem::path directory, std::filesystem::path mca, mcfile::je::Region::ConcatOptions options) {
+    return mcfile::je::Region::ConcatCompressedNbt(rx, rz, directory, mca, options);
+  }
+
   static bool ProcessChunk(mcfile::Dimension dimension, std::filesystem::path mcr, int cx, int cz, std::filesystem::path regionTempDir) {
     using namespace std;
     shared_ptr<mcfile::je::WritableChunk> chunk;
