102efec920d7d52cb5e29a93de823eaabfe9daf1
kbinani <kbinani.bt@gmail.com>
2022-03-20 05:23:34 +0900

    feat: Add item converter

--- include/je2be/box360/item.hpp
+++ include/je2be/box360/item.hpp
@@ -34,13 +34,130 @@ public:
 
 private:
 #pragma region Converter
+  static CompoundTagPtr Log(CompoundTag const &in, CompoundTagPtr &out, int16_t damage) {
+    std::string name;
+    switch (damage) {
+    case 1:
+      name = "spruce_log";
+      break;
+    case 2:
+      name = "birch_log";
+      break;
+    case 3:
+      name = "jungle_log";
+      break;
+    case 0:
+    default:
+      name = "oak_log";
+      break;
+    }
+    out->set("id", String("minecraft:" + name));
+    return out;
+  }
+
+  static CompoundTagPtr Planks(CompoundTag const &in, CompoundTagPtr &out, int16_t damage) {
+    std::string name;
+    switch (damage) {
+    case 1:
+      name = "spruce_planks";
+      break;
+    case 2:
+      name = "birch_planks";
+      break;
+    case 3:
+      name = "jungle_planks";
+      break;
+    case 4:
+      name = "acacia_planks";
+      break;
+    case 5:
+      name = "dark_oak_planks";
+      break;
+    case 0:
+    default:
+      name = "oak_planks";
+      break;
+    }
+    out->set("id", String("minecraft:" + name));
+    return out;
+  }
+
+  static CompoundTagPtr RedSandstone(CompoundTag const &in, CompoundTagPtr &out, int16_t damage) {
+    std::string name = "";
+    switch (damage) {
+    case 1:
+      name = "chiseled_red_sandstone";
+      break;
+    case 2:
+      name = "cut_red_sandstone";
+      break;
+    case 0:
+    default:
+      name = "red_sandstone";
+      break;
+    }
+    out->set("id", String("minecraft:" + name));
+    return out;
+  }
+
   static CompoundTagPtr Same(CompoundTag const &in, CompoundTagPtr &out, int16_t damage) {
+    return out;
+  }
+
+  static CompoundTagPtr Sand(CompoundTag const &in, CompoundTagPtr &out, int16_t damage) {
+    std::string name;
+    switch (damage) {
+    case 1:
+      name = "red_sand";
+      break;
+    case 0:
+    default:
+      name = "sand";
+      break;
+    }
+    out->set("id", String("minecraft:" + name));
+    return out;
+  }
+
+  static CompoundTagPtr Sandstone(CompoundTag const &in, CompoundTagPtr &out, int16_t damage) {
+    std::string name;
+    switch (damage) {
+    case 2:
+      name = "cut_sandstone";
+      break;
+    case 1:
+      name = "chiseled_sandstone";
+      break;
+    case 0:
+    default:
+      name = "sandstone";
+      break;
+    }
+    out->set("id", String("minecraft:" + name));
     return out;
   }
 
   static CompoundTagPtr Stone(CompoundTag const &in, CompoundTagPtr &out, int16_t damage) {
     std::string name = "stone";
     switch (damage) {
+    case 1:
+      name = "granite";
+      break;
+    case 2:
+      name = "polished_granite";
+      break;
+    case 3:
+      name = "diorite";
+      break;
+    case 4:
+      name = "polished_diorite";
+      break;
+    case 5:
+      name = "andesite";
+      break;
+    case 6:
+      name = "polished_andesite";
+      break;
     case 0:
     default:
       break;
@@ -74,6 +191,11 @@ private:
     E(comparator, Same);
     E(daylight_detector, Same);
     E(stone, Stone);
+    E(red_sandstone, RedSandstone);
+    E(sand, Sand);
+    E(sandstone, Sandstone);
+    E(log, Log);
+    E(planks, Planks);
 
 #undef E
     return ret;
