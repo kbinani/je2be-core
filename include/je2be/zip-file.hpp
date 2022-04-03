92ed1e5a0cf2431d8d2b98654800d83d4c793d97
kbinani <kbinani.bt@gmail.com>
2022-04-03 20:31:59 +0900

    feat: ZipFile::Unzip

--- include/je2be/zip-file.hpp
+++ include/je2be/zip-file.hpp
@@ -96,6 +96,100 @@ public:
     return ok;
   }
 
+  static bool Unzip(
+      std::filesystem::path const &input,
+      std::filesystem::path const &output,
+      std::function<bool(uint64_t done, uint64_t total)> progress = [](uint64_t, uint64_t) { return true; }) {
+    using namespace std;
+    namespace fs = std::filesystem;
+
+    void *handle = nullptr;
+    void *stream = nullptr;
+    uint64_t numEntries = 0;
+    uint64_t done = 0;
+
+    handle = mz_zip_create(nullptr);
+    if (!handle) {
+      return false;
+    }
+    defer {
+      mz_zip_delete(&handle);
+    };
+    stream = mz_stream_os_create(nullptr);
+    if (!stream) {
+      return false;
+    }
+    defer {
+      mz_stream_os_delete(&stream);
+    };
+    if (mz_stream_os_open(stream, (char const *)input.u8string().c_str(), MZ_OPEN_MODE_READ) != MZ_OK) {
+      return false;
+    }
+    if (mz_zip_open(handle, stream, MZ_OPEN_MODE_READ) != MZ_OK) {
+      return false;
+    }
+    if (mz_zip_get_number_entry(handle, &numEntries) != MZ_OK) {
+      return false;
+    }
+
+    if (mz_zip_goto_first_entry(handle) != MZ_OK) {
+      // no entry in file
+      return true;
+    }
+    if (!progress(0, numEntries)) {
+      return false;
+    }
+    do {
+      mz_zip_file *fileInfo = nullptr;
+      if (mz_zip_entry_get_info(handle, &fileInfo) != MZ_OK) {
+        return false;
+      }
+      string relFilePath;
+      relFilePath.assign(fileInfo->filename, fileInfo->filename_size);
+      fs::path fullFilePath = output / relFilePath;
+      if (mz_zip_entry_is_dir(handle) == MZ_OK) {
+        Fs::CreateDirectories(fullFilePath);
+        done++;
+        if (progress(done, numEntries)) {
+          continue;
+        } else {
+          break;
+        }
+      }
+      if (mz_zip_entry_read_open(handle, 0, nullptr) != MZ_OK) {
+        return false;
+      }
+      defer {
+        mz_zip_entry_close(handle);
+      };
+      int64_t remaining = fileInfo->uncompressed_size;
+      fs::path parentDir = fullFilePath.parent_path();
+      Fs::CreateDirectories(parentDir);
+      ScopedFile fp(mcfile::File::Open(fullFilePath, mcfile::File::Mode::Write));
+      if (!fp) {
+        return false;
+      }
+      vector<uint8_t> chunk(1024);
+      while (remaining > 0) {
+        int32_t amount = (int32_t)(std::min)(remaining, (int64_t)chunk.size());
+        int32_t read = mz_zip_entry_read(handle, chunk.data(), amount);
+        if (read < 0) {
+          return false;
+        }
+        if (fwrite(chunk.data(), 1, read, fp) != read) {
+          return false;
+        }
+        remaining -= amount;
+      }
+      done++;
+      if (!progress(done, numEntries)) {
+        return false;
+      }
+    } while (mz_zip_goto_next_entry(handle) == MZ_OK);
+
+    return true;
+  }
+
 private:
   void *fHandle;
   void *fStream;
