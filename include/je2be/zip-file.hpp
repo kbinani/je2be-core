1678dff5ce2bc2cd5f6d89dc00c63e47f5317808
kbinani <kbinani.bt@gmail.com>
2022-04-03 20:13:55 +0900

    feat: Add overload for ZipFile::store

--- include/je2be/zip-file.hpp
+++ include/je2be/zip-file.hpp
@@ -35,7 +35,7 @@ public:
     }
     int32_t totalWritten = 0;
     int32_t err = MZ_OK;
-    int32_t remaining = buffer.size();
+    int32_t remaining = (int32_t)buffer.size();
     do {
       int32_t code = mz_zip_entry_write(fHandle, buffer.data() + totalWritten, remaining);
       if (code < 0) {
@@ -45,6 +45,34 @@ public:
         remaining -= code;
       }
     } while (err == MZ_OK && remaining > 0);
+
+    return err == MZ_OK && mz_zip_entry_close(fHandle) == MZ_OK;
+  }
+
+  bool store(mcfile::stream::InputStream &stream, std::string const &filename) {
+    mz_zip_file s = {0};
+    s.version_madeby = MZ_VERSION_MADEBY;
+    s.compression_method = MZ_COMPRESS_METHOD_DEFLATE;
+    s.filename = filename.c_str();
+    int16_t compressLevel = 9;
+    uint8_t raw = 0;
+    if (MZ_OK != mz_zip_entry_write_open(fHandle, &s, compressLevel, raw, nullptr)) {
+      return false;
+    }
+    std::vector<uint8_t> buffer(512);
+    int32_t err = MZ_OK;
+    do {
+      size_t read = stream.read(buffer.data(), buffer.size());
+      if (read == 0) {
+        break;
+      }
+      int32_t code = mz_zip_entry_write(fHandle, buffer.data(), (int32_t)read);
+      if (code < 0) {
+        err = code;
+      } else if (read < buffer.size()) {
+        break;
+      }
+    } while (err == MZ_OK);
 
     return err == MZ_OK && mz_zip_entry_close(fHandle) == MZ_OK;
   }
