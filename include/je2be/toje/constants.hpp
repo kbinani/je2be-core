5785fd57c5327c7b17087d4812c5121a9c000024
kbinani <kbinani.bt@gmail.com>
2022-06-01 06:31:50 +0900

    feat: Update test data 1.19-pre4

--- include/je2be/toje/constants.hpp
+++ include/je2be/toje/constants.hpp
@@ -2,8 +2,8 @@
 
 namespace je2be::toje {
 
-char const *const kVersionString = "1.19 Pre-release 3"; // TODO(1.19)
+char const *const kVersionString = "1.19 Pre-release 4"; // TODO(1.19)
 constexpr int kLevelVersion = 19133;
-constexpr int kDataVersion = 3100; // TODO(1.19): mcfile::je::Chunk::kDataVersion;
+constexpr int kDataVersion = 3101; // TODO(1.19): mcfile::je::Chunk::kDataVersion;
 
 } // namespace je2be::toje
