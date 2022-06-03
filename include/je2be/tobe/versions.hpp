86673eb0e0aabe9f09abb58d6cb19263ace92869
kbinani <kbinani.bt@gmail.com>
2022-06-04 06:41:26 +0900

    feat: Update constants

--- include/je2be/tobe/versions.hpp
+++ include/je2be/tobe/versions.hpp
@@ -2,15 +2,16 @@
 
 namespace je2be::tobe {
 
-int32_t constexpr kBlockDataVersion = 17959425;                               // preview 1.19.10.20
-Version constexpr kMinimumCompatibleClientVersion = Version(1, 19, 10, 0, 0); // preview 1.19.10.20
+int32_t constexpr kBlockDataVersion = 17959425;                               // TODO(1.19): preview 1.19.10.21
+Version constexpr kMinimumCompatibleClientVersion = Version(1, 19, 10, 0, 0); // TODO(1.19): preview 1.19.10.21
 char constexpr kSubChunkVersion = 0x27;
 uint8_t constexpr kSubChunkBlockStorageVersion = 9;
-int32_t constexpr kStorageVersion = 9;           // preview 1.19.10.20
-int32_t constexpr kNetworkVersion = 530;         // preview 1.19.10.20
-char const *const kInventoryVersion = "1.19.10"; // preview 1.19.10.20
+int32_t constexpr kStorageVersion = 9;           // TODO(1.19): preview 1.19.10.21
+int32_t constexpr kNetworkVersion = 532;         // TODO(1.19): preview 1.19.10.21
+char const *const kInventoryVersion = "1.19.10"; // TODO(1.19): preview 1.19.10.21
 uint8_t constexpr kDragonFightVersion = 0;
 
-Version constexpr kSupportVersion = Version(1, 19, 10, 20, 1); // preview 1.19.10.20
+// for lastOpenedWithVersion of level.dat
+Version constexpr kSupportVersion = Version(1, 19, 10, 21, 1); // TODO(1.19): preview 1.19.10.21
 
 } // namespace je2be::tobe
