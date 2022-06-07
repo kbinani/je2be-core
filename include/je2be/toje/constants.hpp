d0f834023bce76799f5e6fa201de5f1131ed932b
kbinani <kbinani.bt@gmail.com>
2022-06-08 04:22:33 +0900

    deps: Update libminecraft-file

--- include/je2be/toje/constants.hpp
+++ include/je2be/toje/constants.hpp
@@ -4,6 +4,6 @@ namespace je2be::toje {
 
 char const *const kVersionString = "1.19";
 constexpr int kLevelVersion = 19133;
-constexpr int kDataVersion = 3105;
+constexpr int kDataVersion = mcfile::je::Chunk::kDataVersion;
 
 } // namespace je2be::toje
