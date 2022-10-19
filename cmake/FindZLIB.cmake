281667e4cc0716ed44c73a3c6af36daa2261e6a9
kbinani <kbinani.bt@gmail.com>
2022-10-20 07:30:14 +0900

    cleanup: Remove unused FindZLIB script

--- cmake/FindZLIB.cmake
+++
@@ -1,7 +0,0 @@
-file(STRINGS "${je2be_zlib_root}/zlib.h" ZLIB_H REGEX "^#define ZLIB_VERSION \"[^\"]*\"$")
-
-string(REGEX REPLACE "^.*ZLIB_VERSION \"([0-9]+).*$" "\\1" ZLIB_VERSION_MAJOR "${ZLIB_H}")
-string(REGEX REPLACE "^.*ZLIB_VERSION \"[0-9]+\\.([0-9]+).*$" "\\1" ZLIB_VERSION_MINOR  "${ZLIB_H}")
-string(REGEX REPLACE "^.*ZLIB_VERSION \"[0-9]+\\.[0-9]+\\.([0-9]+).*$" "\\1" ZLIB_VERSION_PATCH "${ZLIB_H}")
-set(ZLIB_VERSION_STRING "${ZLIB_VERSION_MAJOR}.${ZLIB_VERSION_MINOR}.${ZLIB_VERSION_PATCH}")
-set(ZLIB_FOUND ON)
