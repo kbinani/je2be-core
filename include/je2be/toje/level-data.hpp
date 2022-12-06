f7305c57b69ed1875ae9570a4da3dcea77c3127e
kbinani <kbinani.bt@gmail.com>
2022-12-07 06:07:32 +0900

    fix: Content order in enabled_features

--- include/je2be/toje/level-data.hpp
+++ include/je2be/toje/level-data.hpp
@@ -158,13 +158,13 @@ public:
     }
 
     auto enabledFeatures = List<Tag::Type::String>();
-    enabledFeatures->push_back(String("minecraft:vanilla"));
-    if (ctx.fDataPackBundle) {
-      enabledFeatures->push_back(String("minecraft:bundle"));
-    }
     if (ctx.fDataPack1_20Update) {
       enabledFeatures->push_back(String("minecraft:update_1_20"));
     }
+    if (ctx.fDataPackBundle) {
+      enabledFeatures->push_back(String("minecraft:bundle"));
+    }
+    enabledFeatures->push_back(String("minecraft:vanilla"));
     data->set("enabled_features", enabledFeatures);
 
     auto root = Compound();
