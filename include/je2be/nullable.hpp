ca58d4922626334ac3185accf3dbe2e9608ed9c4
kbinani <kbinani.bt@gmail.com>
2022-12-30 08:32:00 +0900

    fix: Fixed compile error on gcc

--- include/je2be/nullable.hpp
+++ include/je2be/nullable.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <je2be/status.hpp>
+
 #include <variant>
 
 namespace je2be {
--- include/je2be/status.hpp
+++ include/je2be/status.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <filesystem>
+#include <optional>
 #include <string>
 #include <system_error>
 
