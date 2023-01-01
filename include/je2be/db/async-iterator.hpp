a91b8dab9172212cafb1584059204d6f7c05c61b
kbinani <kbinani.bt@gmail.com>
2023-01-02 06:36:33 +0900

    feat: Use Parallel in AsyncIterator

--- include/je2be/db/async-iterator.hpp
+++ include/je2be/db/async-iterator.hpp
@@ -1,8 +1,6 @@
 #pragma once
 
-#include <BS_thread_pool.hpp>
-
-#include <je2be/future-support.hpp>
+#include <je2be/parallel.hpp>
 
 namespace je2be {
 
@@ -20,50 +18,31 @@ public:
     using namespace std;
     using namespace leveldb;
 
-    BS::thread_pool queue(concurrency);
-    deque<future<Accumulator>> futures;
-
-    Accumulator sum = zero;
-
+    vector<char> works(256);
     for (int i = 0; i < 256; i++) {
-      vector<future<Accumulator>> drain;
-      FutureSupport::Drain(concurrency + 1, futures, drain);
-      for (auto &f : drain) {
-        Accumulator r(f.get());
-        join(r, sum);
-      }
-      futures.push_back(queue.submit(Do<Accumulator>, &db, (char)i, zero, accept));
+      works[i] = (char)i;
     }
-    for (auto &f : futures) {
-      Accumulator r(f.get());
-      join(r, sum);
-    }
-    return sum;
-  }
-
-private:
-  template <class Accumulator>
-  static Accumulator Do(
-      leveldb::DB *db,
-      char prefix, Accumulator zero,
-      std::function<void(std::string const &key, std::string const &value, Accumulator &out)> accept) {
-    using namespace std;
-    using namespace leveldb;
-    ReadOptions o;
-    o.fill_cache = false;
-    shared_ptr<Iterator> itr(db->NewIterator(o));
-    Accumulator sum = zero;
-    string p;
-    p.append(1, prefix);
-    for (itr->Seek(Slice(p)); itr->Valid(); itr->Next()) {
-      auto key = itr->key().ToString();
-      if (key[0] != prefix) {
-        break;
-      }
-      auto value = itr->value().ToString();
-      accept(key, value, sum);
-    }
-    return sum;
+    return Parallel::ForEach<Accumulator, char>(
+        works,
+        zero,
+        [&db, zero, accept](char prefix) -> Accumulator {
+          ReadOptions o;
+          o.fill_cache = false;
+          shared_ptr<Iterator> itr(db.NewIterator(o));
+          Accumulator sum = zero;
+          string p;
+          p.append(1, prefix);
+          for (itr->Seek(Slice(p)); itr->Valid(); itr->Next()) {
+            auto key = itr->key().ToString();
+            if (key[0] != prefix) {
+              break;
+            }
+            auto value = itr->value().ToString();
+            accept(key, value, sum);
+          }
+          return sum;
+        },
+        join);
   }
 };
 
