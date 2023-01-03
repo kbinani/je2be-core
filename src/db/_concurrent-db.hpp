2deff99e16e7ddcffa57fc355db7ebfe7fe90080
kbinani <kbinani.bt@gmail.com>
2023-01-03 23:08:42 +0900

    feat: Import ZlibRawTableBuilder

--- src/db/_concurrent-db.hpp
+++ src/db/_concurrent-db.hpp
@@ -185,6 +185,216 @@ class ConcurrentDb : public DbInterface {
     std::map<uintptr_t, std::shared_ptr<Writer>> fWriters;
   };
 
+  class ZlibRawTableBuilder {
+    struct Rep {
+      Rep(const leveldb::Options &opt, leveldb::WritableFile *f)
+          : options(opt),
+            index_block_options(opt),
+            file(f),
+            offset(0),
+            index_block(&index_block_options),
+            num_entries(0),
+            closed(false),
+            pending_index_entry(false) {
+        index_block_options.block_restart_interval = 1;
+        assert(opt.filter_policy == nullptr);
+        assert(opt.compression == leveldb::kZlibRawCompression);
+      }
+
+      leveldb::Options options;
+      leveldb::Options index_block_options;
+      leveldb::WritableFile *file;
+      uint64_t offset;
+      leveldb::Status status;
+      leveldb::BlockBuilder index_block;
+      std::string last_key;
+      int64_t num_entries;
+      bool closed; // Either Finish() or Abandon() has been called.
+
+      // We do not emit the index entry for a block until we have seen the
+      // first key for the next data block.  This allows us to use shorter
+      // keys in the index block.  For example, consider a block boundary
+      // between the keys "the quick brown fox" and "the who".  We can use
+      // "the r" as the key for the index block entry since it is >= all
+      // entries in the first block and < all entries in subsequent
+      // blocks.
+      //
+      // Invariant: r->pending_index_entry is true only if data_block is empty.
+      bool pending_index_entry;
+      leveldb::BlockHandle pending_handle; // Handle to add to index block
+
+      std::string compressed_output;
+    };
+
+  public:
+    // Create a builder that will store the contents of the table it is
+    // building in *file.  Does not close the file.  It is up to the
+    // caller to close the file after calling Finish().
+    ZlibRawTableBuilder(const leveldb::Options &options, leveldb::WritableFile *file)
+        : rep_(new Rep(options, file)) {
+    }
+
+    ZlibRawTableBuilder(const ZlibRawTableBuilder &) = delete;
+    ZlibRawTableBuilder &operator=(const ZlibRawTableBuilder &) = delete;
+
+    // REQUIRES: Either Finish() or Abandon() has been called.
+    ~ZlibRawTableBuilder() {
+      assert(rep_->closed); // Catch errors where caller forgot to call Finish()
+      delete rep_;
+    }
+
+    // Add key,value to the table being constructed.
+    // REQUIRES: key is after any previously added key according to comparator.
+    // REQUIRES: Finish(), Abandon() have not been called
+    void AddAlreadyCompressedAndFlush(const leveldb::Slice &key, const leveldb::Slice &value) {
+      using namespace leveldb;
+      Rep *r = rep_;
+      assert(!r->closed);
+      if (!ok())
+        return;
+      if (r->num_entries > 0) {
+        assert(r->options.comparator->Compare(key, Slice(r->last_key)) > 0);
+      }
+
+      if (r->pending_index_entry) {
+        r->options.comparator->FindShortestSeparator(&r->last_key, key);
+        std::string handle_encoding;
+        r->pending_handle.EncodeTo(&handle_encoding);
+        r->index_block.Add(r->last_key, Slice(handle_encoding));
+        r->pending_index_entry = false;
+      }
+
+      r->last_key.assign(key.data(), key.size());
+      r->num_entries++;
+
+      WriteRawBlock(value, kZlibRawCompression, &r->pending_handle);
+
+      if (ok()) {
+        r->pending_index_entry = true;
+        r->status = r->file->Flush();
+      }
+    }
+
+    // Return non-ok iff some error has been detected.
+    leveldb::Status status() const { return rep_->status; }
+
+    // Finish building the table.  Stops using the file passed to the
+    // constructor after this function returns.
+    // REQUIRES: Finish(), Abandon() have not been called
+    leveldb::Status Finish() {
+      using namespace leveldb;
+      Rep *r = rep_;
+      // Flush();
+      assert(!r->closed);
+      r->closed = true;
+
+      BlockHandle filter_block_handle, metaindex_block_handle, index_block_handle;
+
+      // Write metaindex block
+      if (ok()) {
+        BlockBuilder meta_index_block(&r->options);
+        // TODO(postrelease): Add stats and other meta blocks
+        WriteBlock(&meta_index_block, &metaindex_block_handle);
+      }
+
+      // Write index block
+      if (ok()) {
+        if (r->pending_index_entry) {
+          r->options.comparator->FindShortSuccessor(&r->last_key);
+          std::string handle_encoding;
+          r->pending_handle.EncodeTo(&handle_encoding);
+          r->index_block.Add(r->last_key, Slice(handle_encoding));
+          r->pending_index_entry = false;
+        }
+        WriteBlock(&r->index_block, &index_block_handle);
+      }
+
+      // Write footer
+      if (ok()) {
+        Footer footer;
+        footer.set_metaindex_handle(metaindex_block_handle);
+        footer.set_index_handle(index_block_handle);
+        std::string footer_encoding;
+        footer.EncodeTo(&footer_encoding);
+        r->status = r->file->Append(footer_encoding);
+        if (r->status.ok()) {
+          r->offset += footer_encoding.size();
+        }
+      }
+      return r->status;
+    }
+
+    // Indicate that the contents of this builder should be abandoned.  Stops
+    // using the file passed to the constructor after this function returns.
+    // If the caller is not going to call Finish(), it must call Abandon()
+    // before destroying this builder.
+    // REQUIRES: Finish(), Abandon() have not been called
+    void Abandon() {
+      Rep *r = rep_;
+      assert(!r->closed);
+      r->closed = true;
+    }
+
+    // Number of calls to Add() so far.
+    uint64_t NumEntries() const { return rep_->num_entries; }
+
+    // Size of the file generated so far.  If invoked after a successful
+    // Finish() call, returns the size of the final generated file.
+    uint64_t FileSize() const { return rep_->offset; }
+
+  private:
+    bool ok() const { return status().ok(); }
+    void WriteBlock(leveldb::BlockBuilder *block, leveldb::BlockHandle *handle) {
+      using namespace leveldb;
+      // File format contains a sequence of blocks where each block has:
+      //    block_data: uint8[n]
+      //    type: uint8
+      //    crc: uint32
+      assert(ok());
+      Rep *r = rep_;
+      Slice raw = block->Finish();
+
+      Slice block_contents;
+      CompressionType type = kZlibRawCompression;
+
+      std::string *compressed = &r->compressed_output;
+      mcfile::Compression::CompressDeflate((void *)raw.data(), raw.size(), *compressed, Z_BEST_SPEED);
+      if (compressed->size() < raw.size()) {
+        block_contents = *compressed;
+      } else {
+        // Compression type not supported, or compressed less than equal, so just
+        // store uncompressed form
+        block_contents = raw;
+        type = kNoCompression;
+      }
+
+      WriteRawBlock(block_contents, type, handle);
+      r->compressed_output.clear();
+      block->Reset();
+    }
+
+    void WriteRawBlock(const leveldb::Slice &block_contents, leveldb::CompressionType type, leveldb::BlockHandle *handle) {
+      using namespace leveldb;
+      Rep *r = rep_;
+      handle->set_offset(r->offset);
+      handle->set_size(block_contents.size());
+      r->status = r->file->Append(block_contents);
+      if (r->status.ok()) {
+        char trailer[kBlockTrailerSize];
+        trailer[0] = type;
+        uint32_t crc = crc32c::Value(block_contents.data(), block_contents.size());
+        crc = crc32c::Extend(crc, trailer, 1); // Extend crc to cover block type
+        EncodeFixed32(trailer + 1, crc32c::Mask(crc));
+        r->status = r->file->Append(Slice(trailer, kBlockTrailerSize));
+        if (r->status.ok()) {
+          r->offset += block_contents.size() + kBlockTrailerSize;
+        }
+      }
+    }
+
+    Rep *rep_;
+  };
+
 public:
   explicit ConcurrentDb(std::filesystem::path const &dbname) : fDbName(dbname) {}
 
