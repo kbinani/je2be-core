b2f519c5966d43beb0929d210fccfdaa9ebf60fa
kbinani <kbinani.bt@gmail.com>
2023-01-21 05:20:54 +0900

    feat: Pack LightingModel to 4 bytes

--- src/toje/lighting/_lighting-model.hpp
+++ src/toje/lighting/_lighting-model.hpp
@@ -8,11 +8,30 @@ enum Transparency : uint8_t {
   SOLID = 2,
 };
 
-struct LightingModel {
-  uint32_t fModel = 0;
-  uint8_t fEmission = 0;
-  bool fBehaveAsAirWhenOpenUp = false;
-  Transparency fTransparency = CLEAR;
+union LightingModel {
+  struct {
+    uint32_t fEmission : 4;
+    uint32_t fBehaveAsAirWhenOpenUp : 1;
+    uint32_t fTransparency : 3;
+    uint32_t fModel : 24;
+  };
+  uint32_t fRaw;
+
+  LightingModel(Transparency t) {
+    fTransparency = t;
+    fEmission = 0;
+    fBehaveAsAirWhenOpenUp = false;
+    fModel = 0;
+  }
+
+  LightingModel() {
+    fTransparency = CLEAR;
+    fEmission = 0;
+    fBehaveAsAirWhenOpenUp = false;
+    fModel = 0;
+  }
 };
+
+static_assert(sizeof(LightingModel) == sizeof(uint32_t));
 
 } // namespace je2be::toje::lighting
