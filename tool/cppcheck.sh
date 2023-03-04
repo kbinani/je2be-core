260d65de9f55504fb0b0bbdc0fee2217b8ccfbd8
kbinani <kbinani.bt@gmail.com>
2023-03-05 01:07:11 +0900

    chore: Mark CI as failed when cppcheck founds errors/warnings

--- tool/cppcheck.sh
+++ tool/cppcheck.sh
@@ -2,5 +2,5 @@ set -ue
 
 (
 	cd "$(dirname "$0")/.."
-	bash tool/ls-files | grep -e '^src/' -e '^include/' | cppcheck --file-list=- -I ./include --std=c++20 -j $(nproc) --enable=all --suppressions-list=.cppcheck-suppress
+	bash tool/ls-files | grep -e '^src/' -e '^include/' | cppcheck --file-list=- -I ./include --std=c++20 -j $(nproc) --enable=all --suppressions-list=.cppcheck-suppress --error-exitcode=1
 )
