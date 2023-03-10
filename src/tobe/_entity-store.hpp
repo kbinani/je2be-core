120fcbbbcbbeb13b95d82f6dce1ca4dafea926fb
kbinani <kbinani.bt@gmail.com>
2023-03-11 01:42:31 +0900

    feat: Add EntityStore for temporary entities storage

---
+++ src/tobe/_entity-store.hpp
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <je2be/fs.hpp>
+
+#include "_mem.hpp"
+
+namespace je2be::tobe {
+
+class EntityStore {
+public:
+  static EntityStore *Open(std::filesystem::path const &dir) {
+    using namespace leveldb;
+    DB *db = nullptr;
+    leveldb::Options o;
+    o.compression = kZlibRawCompression;
+    o.create_if_missing = true;
+    if (!DB::Open(o, dir, &db).ok()) {
+      return nullptr;
+    }
+    return new EntityStore(db, dir);
+  }
+
+  ~EntityStore() {
+    if (!fDb) {
+      return;
+    }
+    fDb.reset();
+    Fs::DeleteAll(fDir);
+  }
+
+  Status add(std::vector<CompoundTagPtr> const &entities, Pos2i const &chunk, Pos2i const &fromChunk) {
+    if (!fDb) {
+      return JE2BE_ERROR;
+    }
+    auto stream = std::make_shared<mcfile::stream::ByteStream>();
+    mcfile::stream::OutputStreamWriter writer(stream, mcfile::Endian::Little);
+    for (auto const &e : entities) {
+      if (!CompoundTag::Write(*e, writer)) {
+        return JE2BE_ERROR;
+      }
+    }
+    auto key = Key(chunk, fromChunk);
+    std::string value;
+    stream->drain(value);
+    if (fDb->Put({}, leveldb::Slice(key), leveldb::Slice(value)).ok()) {
+      std::lock_guard<std::mutex> lock(fMut);
+      fChunks.insert(chunk);
+      return Status::Ok();
+    } else {
+      return JE2BE_ERROR;
+    }
+  }
+
+  void entities(Pos2i const &chunk, std::function<void(CompoundTagPtr const &)> cb) {
+    using namespace std;
+    if (!fDb) {
+      return;
+    }
+    leveldb::ReadOptions ro;
+    ro.fill_cache = false;
+    for (int dx = -1; dx <= 1; dx++) {
+      for (int dz = -1; dz <= 1; dz++) {
+        Pos2i fromChunk(chunk.fX + dx, chunk.fZ + dz);
+        auto key = Key(chunk, fromChunk);
+        string value;
+        if (!fDb->Get(ro, leveldb::Slice(key), &value).ok()) {
+          continue;
+        }
+        CompoundTag::ReadUntilEos(value, mcfile::Endian::Little, cb);
+        fDb->Delete({}, leveldb::Slice(key));
+      }
+    }
+  }
+
+private:
+  explicit EntityStore(leveldb::DB *db, std::filesystem::path const &dir) : fDb(db), fDir(dir) {
+  }
+
+  std::string Key(Pos2i const &chunk, Pos2i const &fromChunk) {
+    std::string k;
+    k.resize(16);
+    Mem::Write<i32>(k, 0, chunk.fX);
+    Mem::Write<i32>(k, 4, chunk.fZ);
+    Mem::Write<i32>(k, 8, fromChunk.fX);
+    Mem::Write<i32>(k, 12, fromChunk.fZ);
+    return k;
+  }
+
+public:
+  std::unordered_set<Pos2i, Pos2iHasher> fChunks;
+
+private:
+  std::unique_ptr<leveldb::DB> fDb;
+  std::filesystem::path fDir;
+  std::mutex fMut;
+};
+
+} // namespace je2be::tobe
