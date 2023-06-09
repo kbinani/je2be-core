668343a2d552b819cfab4daca4069758ccc66ebd
kbinani <kbinani.bt@gmail.com>
2023-06-09 19:46:33 +0900

    feat: Update version constants

--- src/tobe/_versions.hpp
+++ src/tobe/_versions.hpp
@@ -4,18 +4,18 @@
 
 namespace je2be::tobe {
 
-i32 constexpr kBlockDataVersion = 18040335;                                   // v1.19.70
-Version constexpr kMinimumCompatibleClientVersion = Version(1, 19, 70, 0, 0); // v1.19.70
-u8 constexpr kSubChunkBlockStorageVersion = 9;                                // v1.19.70
-i32 constexpr kStorageVersion = 10;                                           // v1.19.70
-i32 constexpr kNetworkVersion = 575;                                          // v1.19.70
-char8_t const *const kInventoryVersion = u8"1.19.70";                         // v1.19.70
-u8 constexpr kDragonFightVersion = 0;                                         // v1.19.70
+i32 constexpr kBlockDataVersion = 18087969;                                  // v1.20.0
+Version constexpr kMinimumCompatibleClientVersion = Version(1, 20, 0, 0, 0); // v1.20.0
+u8 constexpr kSubChunkBlockStorageVersion = 9;                               // v1.20.0
+i32 constexpr kStorageVersion = 10;                                          // v1.20.0
+i32 constexpr kNetworkVersion = 589;                                         // v1.20.0
+char8_t const *const kInventoryVersion = u8"1.20.0";                         // v1.20.0
+u8 constexpr kDragonFightVersion = 0;                                        // v1.20.0
 
 // for lastOpenedWithVersion of level.dat
-Version constexpr kSupportVersion = Version(1, 19, 70, 2, 0); // v1.19.70
+Version constexpr kSupportVersion = Version(1, 20, 0, 1, 0); // v1.20.0
 
-char constexpr kChunkVersion = 0x28;          // v1.19.70
+char constexpr kChunkVersion = 0x28;          // v1.20.0
 char constexpr kChunkVersionMaxLegacy = 0x16; // v1.17.41
 
 } // namespace je2be::tobe
--- src/toje/_constants.hpp
+++ src/toje/_constants.hpp
@@ -2,7 +2,7 @@
 
 namespace je2be::toje {
 
-char8_t const *const kVersionString = u8"1.19.4";
+char8_t const *const kVersionString = u8"1.20";
 constexpr int kLevelVersion = 19133;
 constexpr int kDataVersion = mcfile::je::Chunk::kDataVersion;
 constexpr int kDataVersionMaxLegacy = 2730; // 1.17.1
