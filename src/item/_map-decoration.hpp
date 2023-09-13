521de663c92db416e5abfddc7c8f44178e01d370
kbinani <kbinani.bt@gmail.com>
2023-09-13 11:43:06 +0900

    fix: Fix compile error

--- src/item/_map-decoration.hpp
+++ src/item/_map-decoration.hpp
@@ -7,21 +7,6 @@
 namespace je2be {
 
 class MapDecoration : StaticReversibleMap<i8, i32, MapDecoration> {
-  static ReversibleMap<i8, i32> const *CreateTable() {
-    return new ReversibleMap<i8, i32>({
-        {9, 15},  // id = "+", monument
-        {8, 14},  // id = "+", mansion
-        {26, 4},  // id = "+", buried treasure
-        {27, 17}, // village_desert
-        {28, 18}, // village_plains
-        {29, 19}, // village_savanna
-        {30, 20}, // village_snowy
-        {31, 21}, // village_taiga
-        {32, 22}, // jungle_temple
-        {33, 23}, // swamp_hut
-    });
-  }
-
 public:
   static std::optional<i32> BedrockTypeFromJava(i8 type) {
     if (auto be = Forward(type, -1); be > 0) {
@@ -37,6 +22,21 @@ public:
     } else {
       return std::nullopt;
     }
+  }
+
+  static ReversibleMap<i8, i32> const *CreateTable() {
+    return new ReversibleMap<i8, i32>({
+        {9, 15},  // id = "+", monument
+        {8, 14},  // id = "+", mansion
+        {26, 4},  // id = "+", buried treasure
+        {27, 17}, // village_desert
+        {28, 18}, // village_plains
+        {29, 19}, // village_savanna
+        {30, 20}, // village_snowy
+        {31, 21}, // village_taiga
+        {32, 22}, // jungle_temple
+        {33, 23}, // swamp_hut
+    });
   }
 };
 
