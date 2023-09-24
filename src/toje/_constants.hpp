6b5aeb2cec792def34fafdc714b6ac629949294f
kbinani <kbinani.bt@gmail.com>
2023-09-25 04:40:20 +0900

    feat: Update data version

--- src/toje/_constants.hpp
+++ src/toje/_constants.hpp
@@ -2,9 +2,9 @@
 
 namespace je2be::toje {
 
-char8_t const *const kVersionString = u8"1.20";
+char8_t const *const kVersionString = u8"1.20.2";
 constexpr int kLevelVersion = 19133;
-constexpr int kDataVersion = mcfile::je::Chunk::kDataVersion;
+constexpr int kDataVersion = 3578;          // 1.20.2
 constexpr int kDataVersionMaxLegacy = 2730; // 1.17.1
 
 } // namespace je2be::toje
