1a84c3a10c3665283a55e83371560a76268c19b1
kbinani <kbinani.bt@gmail.com>
2023-12-24 03:58:25 +0900

    feat: Update version constants

--- src/toje/_constants.hpp
+++ src/toje/_constants.hpp
@@ -2,9 +2,9 @@
 
 namespace je2be::toje {
 
-char8_t const *const kVersionString = u8"1.20.2";
+char8_t const *const kVersionString = u8"1.20.4";
 constexpr int kLevelVersion = 19133;
-constexpr int kDataVersion = 3578;          // 1.20.2
+constexpr int kDataVersion = 3700;          // 1.20.4
 constexpr int kDataVersionMaxLegacy = 2730; // 1.17.1
 
 } // namespace je2be::toje
