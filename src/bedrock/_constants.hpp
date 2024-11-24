24da2e0d2a4b858c3bbc745d28257fef6599d862
kbinani <kbinani.bt@gmail.com>
2024-11-25 02:06:09 +0900

    feat: Update version constants

--- src/bedrock/_constants.hpp
+++ src/bedrock/_constants.hpp
@@ -2,7 +2,7 @@
 
 namespace je2be::bedrock {
 
-char8_t const *const kVersionString = u8"24w46a"; // TODO(1.21.4)
+char8_t const *const kVersionString = u8"1.21.4 Pre-Release 1"; // TODO(1.21.4)
 constexpr int kLevelVersion = 19133;
 
 } // namespace je2be::bedrock
