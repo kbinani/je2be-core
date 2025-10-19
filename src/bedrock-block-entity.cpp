b1eddfc16a8bf2fcd2b19016859307739b2a3412
kbinani <kbinani.bt@gmail.com>
2025-10-20 07:22:04 +0900

    fix: Remove fall_distance from bee

--- src/bedrock-block-entity.cpp
+++ src/bedrock-block-entity.cpp
@@ -178,7 +178,7 @@ public:
           auto data = Compound();
           for (auto const &key : {u8"Air", u8"ArmorDropChances", u8"ArmorItems", u8"Brain", u8"UUID",
                                   u8"Pos", u8"CanPickUpLoot", u8"CannotEnterHiveTicks", u8"CropsGrownSincePollination", u8"DeathTime",
-                                  u8"FallDistance", u8"HandItems", u8"FallFlying", u8"HurtTime", u8"NoGravity",
+                                  u8"FallDistance", u8"fall_distance", u8"HandItems", u8"FallFlying", u8"HurtTime", u8"NoGravity",
                                   u8"OnGround", u8"PortalCooldown", u8"Rotation"}) {
             result->fEntity->erase(key);
           }
